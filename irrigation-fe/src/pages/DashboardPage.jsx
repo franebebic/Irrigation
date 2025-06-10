af1900f4d983ef250df672309e6e75b9950c79e7
FraneBebić <frane.bebic@gmail.com>
2025-06-10 15:06:27 +0200

    Measurements is comming over MQTT

--- irrigation-fe/src/pages/DashboardPage.jsx
+++ irrigation-fe/src/pages/DashboardPage.jsx
@@ -1,6 +1,6 @@
 export default function DashboardPage() {
   const grafanaURL =
-    "http://192.168.100.11:3000/d/a9ca4341-96a5-478e-ac8f-09196228b72c/moisture-monitoring" +
+    "http://192.168.100.10:3000/d/a9ca4341-96a5-478e-ac8f-09196228b72c/moisture-monitoring" +
     "?orgId=1&from=now-6h&to=now&theme=light&kiosk=tv";
 
   return (
